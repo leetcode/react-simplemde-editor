c62dce12a0f9539cbecdeefe574131f238f3b037
Jeremy Leu <jeremyleu@yahoo.com>
2018-06-18 14:36:14 -0700

    add mentions

--- src/index.js
+++ src/index.js
@@ -50,7 +50,26 @@ module.exports = React.createClass({
     const SimpleMDE = require('simplemde');
     const initialOptions = {
       element: document.getElementById(this.id),
-      initialValue: this.props.value
+      initialValue: this.props.value,
+      autoSuggest: {
+        mode: 'markdown',
+        triggers: {
+          '@': function(stringToTest) {
+            return [
+              { text: 'Thomas ', displayText: 'Thomas' },
+              { text: 'Maria ', displayText: 'Maria' },
+              { text: 'Peter ', displayText: 'Peter' },
+            ];
+          },
+          '#': function(stringToTest) {
+            return [
+              { text: 'Two Sum ', displayText: 'Two Sum' },
+              { text: '3Sum ', displayText: '3Sum' },
+              { text: '4Sum ', displayText: '4Sum' },
+            ];
+          },
+        },
+      },
     };
 
     const allOptions = Object.assign({}, initialOptions, this.props.options);
